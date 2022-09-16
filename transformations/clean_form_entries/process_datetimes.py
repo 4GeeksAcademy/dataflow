2f4cf00e39ec9dc94c9576bf87483bc114f76932
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2022-09-16 16:18:43 +0000

    new update in cleaning process

--- transformations/clean_form_entries/process_datetimes.py
+++ transformations/clean_form_entries/process_datetimes.py
@@ -10,12 +10,13 @@ expected_input = [{
 }]
 
 expected_output = [{
-    'created_at': pd.to_datetime('November 21, 2021'),
-    'updated_at': pd.to_datetime('May 26, 2022'),
-    'won_at': pd.to_datetime('May 26, 2022'),
-    'year-month': pd.to_datetime('2021-11-21').strftime('%Y-%m'),
-    'created_time': pd.to_datetime('2021-11-21').strftime('%H:%M:%S'),
-    'days_to_convert': 186
+    'created_at': pd.to_datetime('2021-11-21 21:32:04'),
+    'updated_at': pd.to_datetime('2022-05-26 20:03:15'),
+    'won_at': pd.to_datetime('2022-05-26 20:03:09'),
+    'year-month': pd.to_datetime('2021-11-21 21:32:04').strftime('%Y-%m'),
+    'Day': pd.to_datetime('2021-11-21 21:32:04').strftime('%B %d, %Y'),
+    'created_time': pd.to_datetime('2021-11-21 21:32:04').strftime('%H:%M:%S'),
+    'days_to_convert': 185
 }]
 
 
@@ -30,19 +31,20 @@ def run(df):
     df['updated_at'] = df['updated_at'].apply(pd.to_datetime)
     df['won_at'] = df['won_at'].apply(pd.to_datetime)
 
+    #creating new columns
+    df['year-month'] = df['created_at'].dt.strftime('%Y-%m')
+    df['Day'] = df['created_at'].dt.strftime('%B %d, %Y')
+    df['created_time'] = df['created_at'].dt.strftime('%H:%M:%S')
+    df['days_to_convert'] = (df['won_at'] - df['created_at']).dt.days.abs()
+    
     #Changing format
-    df['created_at'] = df['created_at'].dt.strftime('%B %d, %Y')
-    df['updated_at'] = df['updated_at'].dt.strftime('%B %d, %Y')
-    df['won_at'] = df['won_at'].dt.strftime('%B %d, %Y')
+    df['created_at'] = df['created_at'].dt.strftime('%Y-%m-%d %H:%M:%S')
+    df['updated_at'] = df['updated_at'].dt.strftime('%Y-%m-%d %H:%M:%S')
+    df['won_at'] = df['won_at'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
     #Format change also changed the column type to object, so we need to convert it to datetime again 
     df['created_at'] = df['created_at'].apply(pd.to_datetime)
     df['updated_at'] = df['updated_at'].apply(pd.to_datetime)
     df['won_at'] = df['won_at'].apply(pd.to_datetime)
-
-    #creating new columns
-    df['year-month'] = df['created_at'].dt.strftime('%Y-%m')
-    df['created_time'] = df['created_at'].dt.strftime('%H:%M:%S')
-    df['days_to_convert'] = (df['won_at'] - df['created_at']).dt.days.abs()
     
     return df
