2d0613c1edbffa4ff14ae55ef4a976f2ef208124
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2022-10-04 22:03:37 +0000

    update, not solved yet in correct_location

--- transformations/clean_form_entries/drop_useless.py
+++ transformations/clean_form_entries/drop_useless.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 TO_DROP = ['phone','client_comments','street_address','city','latitude','longitude','state',
                   'zip_code','referral_key','browser_lang','ac_expected_cohort','current_download','utm_content',
-                  'storage_status','user_id','ac_contact_id','first_name','last_name']
+                  'storage_status','user_id','ac_contact_id','first_name','last_name','storage_status_text']
 
 # list of values in columns
 ph= ['ab','ba','cb','bc'] 
