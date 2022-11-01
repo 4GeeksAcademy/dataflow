2b9661bb8a1c9525343edc131953609d39d25ca1
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2022-11-01 00:08:24 +0000

    dropping 4geeks emails in a new transformation

--- pipelines/clean_form_entries/drop_test_rows.py
+++ pipelines/clean_form_entries/drop_test_rows.py
@@ -2,20 +2,23 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-mediums= ['ab', 'xxxxx','bc'] 
-sources= ['ab','bc','fintech'] 
+mediums= ['ab', 'xxxxx','bc','cd'] 
+sources= ['ab','bc','fintech','cd'] 
+emails= ['a@4geeksacademy.com', 'd@gmail.com', 'bc', 'c@gmail.com']
 
-dict = {'utm_medium':mediums,'utm_source':sources}
+
+dict = {'utm_medium':mediums,'utm_source':sources, 'email':emails}
 
 #expected input as dataframe
 expected_input = pd.DataFrame(dict)
 
 #values for Output
-output_mediums = ['ab']
-output_sources = ['ab']
+output_mediums = ['cd']
+output_sources = ['cd']
+output_emails = ['c@gmail.com']
 
 #expected output
-expected_output = pd.DataFrame({'utm_medium':output_mediums, 'utm_source':output_sources})
+expected_output = pd.DataFrame({'utm_medium':output_mediums, 'utm_source':output_sources,'email':output_emails})
 
 def run(df):
     """
@@ -24,6 +27,9 @@ def run(df):
     """
 
     print('Shape before drop_test_rows', df.shape)
+
+    # Drop testing rows
+    df = df[df["email"].str.contains("@4geeks") == False]
 
     #Do not keep rows that were only for test (identify them even if they do not have @4geeks email)
     df = df[df['utm_source'] != 'test_s']
--- pipelines/clean_form_entries/drop_useless.py
+++ pipelines/clean_form_entries/drop_useless.py
@@ -6,44 +6,32 @@ TO_DROP = ['client_comments','street_address','city','latitude','longitude','sta
                   'zip_code','referral_key','browser_lang','ac_expected_cohort','current_download','utm_content',
                   'storage_status','user_id','ac_contact_id','first_name','last_name','storage_status_text']
 
-# list of values in columns
+expected_input = [{
+    'client_comments':'ab',
+    'street_address':'ab',
+    'city':'ab',
+    'latitude':'ab',
+    'longitude':'ab',
+    'state':'ab',
+    'zip_code':'ab',
+    'referral_key':'ab',
+    'browser_lang':'ab',
+    'ac_expected_cohort':'ab',
+    'current_download':'ab',
+    'utm_content':'ab',
+    'storage_status':'ab',
+    'user_id':'ab',
+    'ac_contact_id':'ab',
+    'first_name':'ab',
+    'last_name':'ab',
+    'storage_status_text':'ab',
+    'email':'d@gmail.com'
+}]
 
-cmts= ['ab','ba','cb','bc'] 
-adrs= ['ab','ba','cb','bc'] 
-cty= ['ab','ba','cb','bc'] 
-ltde= ['ab','ba','cb','bc'] 
-lgde= ['ab','ba','cb','bc']  
-state= ['ab','ba','cb','bc'] 
-zip= ['ab','ba','cb','bc']  
-key= ['ab','ba','cb','bc']  
-lang= ['ab','ba','cb','bc']  
-ac_cohort= ['ab','ba','cb','bc']  
-download= ['ab','ba','cb','bc'] 
-utm_content= ['ab','ba','cb','bc'] 
-status= ['ab','ba','cb','bc'] 
-user_id= ['ab','ba','cb','bc'] 
-ac_contact_id= ['ab','ba','cb','bc']
-first_name= ['ab','ba','cb','bc']
-last_name= ['ab','ba','cb','bc'] 
-storage_st_txt= ['ab','ab','ab','ab']
-ids= [None, None, None, None] 
-emails= ['a@4geeks.co', 'd@gmail.com', 'b@4geeksacademy.com', 'c@gmail.com']
- 
-# dictionary of lists 
-dict = {'client_comments':cmts,'street_address':adrs,'city':cty,'latitude':ltde,'longitude':lgde,'state':state,
-                  'zip_code':zip,'referral_key':key,'browser_lang':lang,'ac_expected_cohort':ac_cohort,'current_download':download,
-                  'utm_content':utm_content,'storage_status':status,'user_id':user_id,'ac_contact_id':ac_contact_id,
-                  'first_name':first_name,'last_name':last_name, 'storage_status_text': storage_st_txt, 'ids':ids, 'email':emails} 
+expected_output = [{
+    'email':'d@gmail.com'
+}]
 
-#expected input as dataframe
-expected_input = pd.DataFrame(dict)
-
-#values for Output
-output_emails = ['d@gmail.com', 'c@gmail.com']
-
-#expected output
-expected_output = pd.DataFrame({'email':output_emails})
-    
 
 def run(df):
     """
@@ -59,9 +47,6 @@ def run(df):
     # Drop null columns
     df.dropna(axis=1, how='all', inplace=True)
 
-    # Drop testing rows
-    df = df[df["email"].str.contains("@4geeks") == False]
-    
     print('Shape after drop_useless ', df.shape)
 
     return df
